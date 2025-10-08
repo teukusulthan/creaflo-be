8d319a69c9dcb2a83fdd8c791f7e185ee5398793
teukusulthan <teukusultanul@gmail.com>
2025-10-08 09:26:50 +0700

    feat(auth): add authenticate user before using ai endpoints

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,20 @@ import { corsMiddleware } from "./middlewares/cors";
 import { errorHandler } from "./middlewares/errorHandler";
 import authRoute from "./routes/auth.routes";
 import aiRoutes from "./routes/ai.routes";
+import { authenticate } from "./middlewares/auth";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //middlewares
+app.use(cookieParser());
 app.use(corsMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 
 app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/ai", aiRoutes);
+app.use("/api/v1/ai", authenticate, aiRoutes);
 
 app.use(errorHandler);
 
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,17 +2,26 @@ import { Request, Response, NextFunction } from "express";
 import { verifyToken } from "../utils/jwt";
 import { AppError } from "../utils/appError";
 
-export function authenticate(req: Request, res: Response, next: NextFunction) {
+export function authenticate(req: Request, _res: Response, next: NextFunction) {
   const header = req.headers.authorization;
-  if (!header?.startsWith("Bearer")) {
-    throw new AppError(401, "No token");
+  const cookieToken = req.cookies?.token;
+  let token: string | undefined;
+
+  if (header?.startsWith("Bearer ")) {
+    token = header.slice(7);
+  } else if (cookieToken) {
+    token = cookieToken;
+  }
+
+  if (!token) {
+    throw new AppError(401, "No token provided");
   }
 
   try {
-    const decoded = verifyToken(header.slice(7));
+    const decoded = verifyToken(token);
     (req as any).user = decoded;
     next();
   } catch {
-    throw new AppError(401, "Invalid or exppired token");
+    throw new AppError(401, "Invalid or expired token");
   }
 }
